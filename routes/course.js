92ce0484ff9ef9461f36ca26d0f81fe092d5247e
Abhi <abhidadhaniya23@gmail.com>
2021-09-20 00:36:44 +0530

    fix some minor bugs

--- routes/course.js
+++ routes/course.js
@@ -1,35 +1,26 @@
 const express = require('express')
 const chapter = express.Router()
-const _ = require('lodash')
 // const path = require('path')
 const chapterJsonData = require('../JSON_DATA_CUSTOMIZING/chapters.json')
 const bmeJsonData = require('../JSON_DATA_CUSTOMIZING/bme_data.json')
-// const { map } = require('lodash')
 
-// chapter.use(express.static('public'))
-
-// _.uniqBy(chapters, 'chapter')
 function getUniqueListBy(arr, key) {
     return [...new Map(arr.map(item => [item[key], item])).values()]
 }
-// console.log("Unique by chapters")
-// console.log(chapterArr)
-
-// function getUniqueListBy(arr, key) {
-//     return [...new Map(arr.map(item=>[item[key],item])).values()]
-// }
-// const 
-
-// chapters = [...new Set(chapters)]
-// console.log(chapters);
-
-// let objArr={
-//     [
-
-//     ]
-// }
-
+// console.log(bmeJsonData.length);
 chapter.get('/', async (req, res) => {
+    let chaptersArr = []
+    // let chaptersArr = bmeJsonData.filter(chapter => chapter.chapter)
+
+    bmeJsonData.forEach(value => {
+        if (chaptersArr.indexOf(value) == -1) {
+            chaptersArr.push(value)
+        }
+    })
+    console.log(chaptersArr.chapter);
+
+    // console.log(bmeJsonData[0].chapter==bmeJsonData[0].chapter);
+    // console.log(chaptersArr);
     let chapters = []
     bmeJsonData.forEach(element => {
         let obj = {}
@@ -37,16 +28,34 @@ chapter.get('/', async (req, res) => {
         obj.id = element.id
         chapters.push(obj)
     })
-    const chaptersArr = getUniqueListBy(chapters, 'chapter')
-    console.log(chaptersArr);
+    const chaptersArr2 = getUniqueListBy(chapters, 'chapter')
+    // console.log(chaptersArr);
 
     await res.render('index.ejs', {
-        // chapterJsonData: chapterJsonData,
         bmeJsonData: bmeJsonData,
-        chaptersArr: chaptersArr
+        chaptersArr: chaptersArr2
     })
 })
-chapter.get('/topic/:topic', async (req, res) => {
+chapter.get('/:chapterName/:topicId', (req, res) => {
+    try {
+        let chapter = bmeJsonData.find(chapter => chapter.chapter == req.params.chapterName)
+        let topic = bmeJsonData.find(topic => topic.id == req.params.topicId)
+        // console.log(chapter.chapter);
+        // console.log(topic);
+        res.render('chapterTopic.ejs', {
+            bmeJsonData: bmeJsonData,
+            chapterName: req.params.chapterName,
+            topicName: topic.id,
+            topicContent: topic.content,
+            topicTitle: topic.title,
+            topicYoutubeLink: topic.youtubeLink
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+/* chapter.get('/topic/:topic', async (req, res) => {
     try {
         let topic = await bmeJsonData.find(topic => topic.id === req.params.topic)
         res.render('chapterTopic.ejs', {
@@ -95,6 +104,6 @@ chapter.get('/:id', async (req, res) => {
     // } catch (error) {
     //     console.log(error);
     // }
-})
+}) */
 
 module.exports = chapter
