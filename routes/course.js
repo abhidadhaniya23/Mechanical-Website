d3922902edf11338a3cbae1e1ba3636619db96da
Abhi <abhidadhaniya23@gmail.com>
2021-09-22 01:26:04 +0530

    fix some bugs and remove unused codes

--- routes/course.js
+++ routes/course.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const chapter = express.Router()
-// const path = require('path')
 const chapterJsonData = require('../JSON_DATA_CUSTOMIZING/chapter_data.json')
 const bmeJsonData = require('../JSON_DATA_CUSTOMIZING/bme_data.json')
 
-// console.log(bmeJsonData.length);
 chapter.get('/', async (req, res) => {
     try {
         await res.render('index.ejs', {
@@ -36,55 +34,5 @@ chapter.get('/:chapterName/:topicId', (req, res) => {
         console.log(error);
     }
 })
-/* chapter.get('/topic/:topic', async (req, res) => {
-    try {
-        let topic = await bmeJsonData.find(topic => topic.id === req.params.topic)
-        res.render('chapterTopic.ejs', {
-            chapterJsonData: chapterJsonData,
-            bmeJsonData: bmeJsonData,
-            chapterName: req.params.topic,
-            topicName: topic,
-            topicContent: topic.content,
-            topicTitle: topic.title,
-            topicYoutubeLink: topic.youtubeLink
-        })
-    } catch (error) {
-        console.log(error);
-    }
-})
-// console.log(bmeJsonData[0].chapter)
-chapter.get('/:id', async (req, res) => {
-    let firstTopicLink;
-    try {
-        for (let index = 0; index < bmeJsonData.length; index++) {
-            if (bmeJsonData[index].chapter.replace(/ /g, '-') == req.params.id) {
-                firstTopicLink = bmeJsonData[index].youtubeLink;
-                // console.log(bmeJsonData[index].chapter.replace(/ /g, '-'))
-                // console.log(req.params.id)
-                break;
-            }
-            // console.log(firstTopicLink);
-        }
-        // console.log(bmeJsonData)
-        res.render('chapterTopic.ejs', {
-            bmeJsonData: bmeJsonData,
-            chapterName: req.params.id,
-            topicYoutubeLink: firstTopicLink
-        })
-        // console.log(firstTopicLink);
-    } catch (error) {
-        console.log(error);
-    }
-    // try {
-    //     let chapter = await chapterJsonData.find(chapter => chapter.id === req.params.id)
-    //     await res.render('chapterTopic.ejs', {
-    //         content: chapter.content,
-    //         title: chapter.title,
-    //         youtubeLink: chapter.youtubeLink
-    //     })
-    // } catch (error) {
-    //     console.log(error);
-    // }
-}) */
 
 module.exports = chapter
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const app = express()
-// const path = require('path')
 const port = process.env.PORT || 3000
 
+// static Files
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-// app.use('/css', express.static('public/css'))
-// app.use('/img', express.static('public/img'))
 
 // Routes
-const chapter = require('./routes/chapter')
 const course = require('./routes/course')
 
-// app.use('/chapter', chapter)
 app.use('/', course)
-
-// app.get('/', (req, res) => {
-// 	res.render('index.ejs')
-// })
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
